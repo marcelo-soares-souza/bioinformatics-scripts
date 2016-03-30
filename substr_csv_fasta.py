69db9d54886430e7316155ea61d2d61215bad01c
Marcelo Soares Souza <marcelo@libertais.org>
2016-03-30 16:12:29 -0300

    Improved output messages

--- substr_csv_fasta.py
+++ substr_csv_fasta.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 
 import os
 import sys
@@ -11,29 +11,34 @@ from Bio.Alphabet import Alphabet
 if len(sys.argv) < 2:
     print('Usage: ', str(sys.argv[0]), '[CSV FILE] [FASTA FILE]')
 else:
+    csv_file = str(sys.argv[1])
+    fasta_file = str(sys.argv[2])
 
-    records = SeqIO.index(str(sys.argv[2]), 'fasta')
-    output = open(os.path.splitext(sys.argv[2])[0] + '.result.fasta', "w")
+    records = SeqIO.index(fasta_file, 'fasta')
+    output = open(os.path.splitext(sys.argv[2])[0] + '.result.fasta', 'w')
 
-    with open(str(sys.argv[1]), 'r') as csv_file:
+    print('Using', csv_file, 'CSV Table and', fasta_file, 'FASTA File')
+
+    with open(csv_file, 'r') as csv_file:
         reader = csv.reader(csv_file)
 
         for id, begin, end in reader:
-            print('Processing', id, 'of size', len(records[id]), 'starting in', begin, 'ending', end)
+            print('\nProcessing', id, 'of size', len(records[id]), 'starting in', begin, 'ending', end)
 
-            if begin < end:
+            if int(begin) < int(end):
+                print('Normal Slicing...')
                 seq = records[id].seq[int(begin):int(end)]
             else:
+                print('Reverse Complement...')
                 seq = records[id].seq[int(end):int(begin)].reverse_complement()
 
             header = '%s|size%s[%s_to_%s](%s nts)' % (id, len(records[id]), begin, end, len(seq))
 
             record = SeqRecord(Seq(str(seq), Alphabet()), id=str(header), description='')
 
-            SeqIO.write(record, output, "fasta")
+            SeqIO.write(record, output, 'fasta')
 
-    print('Result in ', os.path.splitext(sys.argv[2])[0] + '.result.fasta')
+    print('\nCheck the results in ', os.path.splitext(sys.argv[2])[0], '.result.fasta\n', sep='')
 
     output.close()
     records.close()
-
