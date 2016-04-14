3a23c9fc5e8caf68d7170446ea0169ae0338f236
Marcelo Soares Souza <marcelo.soares@colaborador.embrapa.br>
2016-04-14 13:15:56 -0300

    Get Substrings in a FASTA based in the LTR Finder output

---
+++ substr_ltrfinder_fasta.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import csv
+import itertools
+from time import time
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio.Alphabet import Alphabet
+
+if len(sys.argv) < 3:
+    print('Usage: ', str(sys.argv[0]), '[LTR FINDER FILE] [FASTA FILE]')
+else:
+    start_time = time()
+
+    ltrfinder_file = str(sys.argv[1])
+    fasta_file = str(sys.argv[2])
+
+    records = SeqIO.index(fasta_file, 'fasta')
+    output = open(os.path.splitext(sys.argv[2])[0] + '.result.fasta', 'w')
+
+    print('Using', ltrfinder_file, 'LTR Finder and', fasta_file, 'FASTA File')
+
+    with open(ltrfinder_file, 'r') as file:
+        for line1, line2, line3 in itertools.zip_longest(*[file]*3):
+            id = line1.split()[1]
+            begin = line2.split()[2]
+            end = line2.split()[4]
+
+            print('\nProcessing', id, 'of size', len(records[id]), 'starting in', begin, 'ending', end)
+
+            if int(begin) < int(end):
+                print('Normal Slicing...')
+                seq = records[id].seq[int(begin):int(end)]
+            else:
+                print('Reverse Complement...')
+                seq = records[id].seq[int(end):int(begin)].reverse_complement()
+
+            header = '%s|size%s[%s_to_%s](%s nts)' % (id, len(records[id]), begin, end, len(seq))
+
+            record = SeqRecord(Seq(str(seq), Alphabet()), id=str(header), description='')
+
+            SeqIO.write(record, output, 'fasta')
+
+    print('\nCheck the results in ', os.path.splitext(sys.argv[2])[0], '.result.fasta\n', sep='')
+
+    output.close()
+    records.close()
+
+    end_time = time()
+
+    print('Took %.3f seconds...\n' % (end_time - start_time))
