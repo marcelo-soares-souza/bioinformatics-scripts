25934e3c1f12660c561aacbae65d1ccf792c7881
Marcelo Soares Souza <marcelo.soares@colaborador.embrapa.br>
2016-04-20 14:57:42 -0300

    Improvemente to accept full table in CSV to remove_sequences_csv_fasta.py

--- remove_sequences_csv_fasta.py
+++ remove_sequences_csv_fasta.py
@@ -10,13 +10,16 @@ from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from Bio.Alphabet import Alphabet
 
-if len(sys.argv) < 3:
-    print('Usage:', str(sys.argv[0]), '[CSV FILE] [FASTA FILE]')
+if len(sys.argv) < 5:
+    print('Usage:', str(sys.argv[0]), '[CSV FILE] [FASTA FILE] [PIDENT] [QCOVS]')
 else:
     start_time = time()
 
     csv_filename = str(sys.argv[1])
     fasta_filename = str(sys.argv[2])
+    arg_pident = float(sys.argv[3])
+    arg_qcovs = float(sys.argv[4])
+
     output_result_filename = os.path.splitext(sys.argv[2])[0] + '.result.' + datetime.datetime.now().strftime('%Y%m%d') + '.fasta'
     output_removed_filename = os.path.splitext(sys.argv[2])[0] + '.removed.' + datetime.datetime.now().strftime('%Y%m%d') + '.fasta'
 
@@ -31,26 +34,33 @@ else:
     print('\nUsing', csv_filename, 'CSV Table and', fasta_filename, 'FASTA File')
 
     with open(csv_filename, 'r') as csv_file:
-        reader = csv.reader(csv_file)
+        reader = csv.reader(csv_file, delimiter='\t')
 
-        for id in reader:
-            id = id[0]
-            if id in records.keys():
-              print('\nRemoving', id)
+        for data in reader:
+            print(data)
+            id = data[0]
+            pident = float(data[11])
+            qcovs = float(data[14])
 
-              SeqIO.write(records[id], output_removed, 'fasta')
+            print('\nChecking', id, 'with PIDENT', pident, 'and QCOVS', qcovs)
 
-              del records[id]
+            if pident >= arg_pident and qcovs >= arg_qcovs:
+                if id in records.keys():
+                    print('\nRemoving', id, 'with PIDENT', pident, 'and QCOVS', qcovs)
 
-              write = True
-              removed_sequences = removed_sequences + 1
+                    SeqIO.write(records[id], output_removed, 'fasta')
+
+                    del records[id]
+
+                    write = True
+                    removed_sequences = removed_sequences + 1
 
     if write:
         output = open(output_result_filename, 'w')
         [ SeqIO.write(record, output, 'fasta') for (id, record) in records.items() ]
         output.close()
         print('\nCheck the results in ', output_result_filename, '\n', sep='')
-        print('\nRemoved Sequences (',removed_sequences,') in ', output_removed_filename, '\n', sep='')
+        print('Removed Sequences (',removed_sequences,') in ', output_removed_filename, '\n', sep='')
     else:
         print('No sequences found\n')
 
