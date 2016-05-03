8dcdd9a97460d43ec1e2eac9f7c797417692aaaa
Marcelo Soares Souza <marcelo.soares@colaborador.embrapa.br>
2016-05-03 16:52:48 -0300

    Output With and Without keyword output fastq result

--- cleanup_sequences_keyword.py
+++ cleanup_sequences_keyword.py
@@ -18,22 +18,31 @@ else:
     fastq_filename = str(sys.argv[1])
     keyword = str(sys.argv[2])
 
-    output_result_filename = os.path.splitext(sys.argv[1])[0] + '.without_' + keyword + '.fastq'
+    filename = {}
+    filename['without_keyword'] = os.path.splitext(sys.argv[1])[0] + '.without_' + keyword + '.fastq'
+    filename['with_keyword'] = os.path.splitext(sys.argv[1])[0] + '.with_' + keyword + '.fastq'
 
     print('\nReading', fastq_filename, 'FASTQ File')
 
     records = SeqIO.to_dict(SeqIO.parse(fastq_filename, 'fastq'))
 
+    output = {}
+    output['with_keyword'] = open(filename['with_keyword'], 'w')
     for record in list(records.items()):
         if keyword.lower() in record[1].description.lower():
             print('\nRemoving', record[1].description)
+
+            SeqIO.write(record[1], output['with_keyword'], 'fastq')
+
             del records[record[0]]
 
-    output = open(output_result_filename, 'w')
-    [SeqIO.write(record, output, 'fastq') for (id, record) in records.items()]
-    output.close()
+    output['with_keyword'].close()
 
-    print('\nCheck the results in ', output_result_filename, '\n', sep='')
+    output['without_keyword'] = open(filename['without_keyword'], 'w')
+    [SeqIO.write(record, output['without_keyword'], 'fastq') for (id, record) in records.items()]
+    output['without_keyword'].close()
+
+    print('\nCheck the results in ', filename['without_keyword'], ' and ', filename['with_keyword'],'\n', sep='')
 
     end_time = time()
 
