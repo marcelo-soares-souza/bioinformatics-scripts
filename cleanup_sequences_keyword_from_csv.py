1d3090082914ed20c6a170a62a59f53d5498c6b3
Marcelo Soares Souza <marcelo.soares@colaborador.embrapa.br>
2016-05-23 16:54:22 -0300

    New script cleanup_sequences_keyword_from_csv.py by Brenda, Thais and Marcelo

---
+++ cleanup_sequences_keyword_from_csv.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import csv
+import datetime
+from time import time
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio.Alphabet import Alphabet
+
+if len(sys.argv) < 3:
+    print('Usage:', str(sys.argv[0]), '[FASTA FILE] [CSV FILE]')
+else:
+    start_time = time()
+
+    fasta_filename = str(sys.argv[1])
+    csv_filename = str(sys.argv[2])
+
+    filename = {}
+    filename['without_keyword'] = os.path.splitext(sys.argv[1])[0] + '.without' + '.fasta'
+    filename['with_keyword'] = os.path.splitext(sys.argv[1])[0] + '.with' + '.fasta'
+
+    print('\nReading', fasta_filename, 'FASTA File')
+
+    records = SeqIO.to_dict(SeqIO.parse(fasta_filename, 'fasta'))
+
+    output = {}
+    output['with_keyword'] = open(filename['with_keyword'], 'w')
+
+    with open(csv_filename, 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        for keyword in reader:
+            kw = ''.join(keyword)
+            for record in list(records.items()):
+                if kw.lower() in record[1].description.lower():
+                   print('\nRemoving', record[1].description)
+                   SeqIO.write(record[1], output['with_keyword'], 'fasta')
+                   del records[record[0]]
+
+    output['with_keyword'].close()
+
+    output['without_keyword'] = open(filename['without_keyword'], 'w')
+    [SeqIO.write(record, output['without_keyword'], 'fasta') for (id, record) in records.items()]
+    output['without_keyword'].close()
+
+    print('\nCheck the results in ', filename['without_keyword'], ' and ', filename['with_keyword'],'\n', sep='')
+
+    end_time = time()
+
+    print('\nTook %.3f seconds...\n' % (end_time - start_time))
