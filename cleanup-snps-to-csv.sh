f07d31c52dd5b7c641a3e75348086e022a19372c
Marcelo Soares Souza <marcelo.soares@colaborador.embrapa.br>
2016-10-24 15:20:40 -0200

    Minor Fix

--- cleanup-snps-to-csv.sh
+++ cleanup-snps-to-csv.sh
@@ -3,5 +3,5 @@
 for FILE in `ls *.vcf`; do
   FILENAME="${FILE%.*}"
   echo -e "\n${FILENAME}\n"
-  grep -v "^#" $FILE | cut -d$'\t' -f1,2 | sort | uniq > ${FILENAME}.csv
+  grep -v "^#" $FILE | cut -d$'\t' -f1,2,4,5 | sort -u -t$'\t' -k1,1 -k 2,2 | uniq > ${FILENAME}.csv
 done
